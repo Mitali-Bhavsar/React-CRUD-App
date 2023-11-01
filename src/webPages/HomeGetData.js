60d44d54b6376a4ed58c018fdc5194197f76d776
Mitali-Bhavsar <mitali.bodkhe@gmail.com>
2023-11-02 02:10:37 +0530

    HomeGetData.js dependency added

--- src/webPages/HomeGetData.js
+++ src/webPages/HomeGetData.js
@@ -7,7 +7,7 @@ const useHomeGetData = (url) => {
         .then((res)=>res.json())
         .then((data)=>setData(data))
         .catch((err)=>console.log(err))
-    })
+    },[])
   return data;
 }
 export default useHomeGetData;
