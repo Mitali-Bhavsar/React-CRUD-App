9cb23d83896104d09c7e7fc8d1ebcbc0223c8f9e
Mitali-Bhavsar <mitali.bodkhe@gmail.com>
2023-11-02 15:54:50 +0530

    Dependency url added in HomeGetData.js

--- src/webPages/HomeGetData.js
+++ src/webPages/HomeGetData.js
@@ -2,12 +2,14 @@ import { useEffect, useState } from 'react'
 
 const useHomeGetData = (url) => {
     const [data,setData] = useState([]);
+
     useEffect(()=>{
-        fetch(url)
-        .then((res)=>res.json())
-        .then((data)=>setData(data))
-        .catch((err)=>console.log(err))
-    },[])
+      fetch(url)
+      .then((res)=>res.json())
+      .then((data)=>setData(data))
+      .catch((err)=>console.log(err))
+    },[url])
+  
   return data;
 }
 export default useHomeGetData;
